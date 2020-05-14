82ddc3fd0fa55168d41dae37582308d0cfcb77ed
Elshubi <55143450+Elshubi@users.noreply.github.com>
2020-05-14 09:59:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //Date selector
 
-var Date = new Date();
+var d = new Date();
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'];
-document.querySelector('.date').innerHTML = Date.getDate() + '      ' + months[Date.getMonth()] + '     ' + Date.getFullYear();
+document.querySelector('.date').innerHTML = d.getDate() + '      ' + months[d.getMonth()] + '     ' + d.getFullYear();
+
+//clock
+
+setInterval(disptime, 1000);
+
+function disptime() {
+  var t = new Date();
+  var hours = t.getHours();
+  var minutes = t.getMinutes();
+  var seconds = t.getSeconds();
+  var meridian = "AM"
+  hours < 12 ? meridian = 'AM' : meridian = 'PM';
+  minutes < 10 ? minutes = '0' + minutes : minutes = minutes;
+  seconds < 10 ? seconds = '0' + seconds : seconds = seconds;
+  document.querySelector('.time').textContent = hours + ':' + minutes + ':' + seconds + ' ' + meridian;
+  };
+
+   
+
 
 // Add new Item 
 var newText = document.querySelector('.newItemText');
@@ -38,7 +57,7 @@ function addnew() {
     e.target.innerHTML='';
 	} else if 
     (e.target && e.target.matches("p") && highlight.checked) {
-      e.target.style.backgroundColor= 'red';
+      e.target.style.backgroundColor= 'rgb(255, 32, 51)';
   }
   
   });
